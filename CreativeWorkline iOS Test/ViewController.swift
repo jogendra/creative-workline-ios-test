3532cbf69f362a13e931b3e522b672eef97282be
imjog <imjog24@gmail.com>
2018-02-17 00:08:51 +0530

    Configured MFMailController and open URL in browser

--- CreativeWorkline iOS Test/ViewController.swift
+++ CreativeWorkline iOS Test/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import MessageUI
 
 class ViewController: UIViewController {
 
@@ -55,30 +56,75 @@ class ViewController: UIViewController {
     
     private func makePartOfStringTextItallic() {
         
-        let descriptionText: NSString = (descriptionTextView.text as? NSString)!
-        let strText = NSMutableAttributedString(string: descriptionTextView.text)
-        strText.addAttribute(NSAttributedStringKey.font, value: UIFont.italicSystemFont(ofSize: 12.0), range: descriptionText.range(of: "tagging the world!"))
-        descriptionTextView.attributedText = strText
+        let descriptionText: NSString = (descriptionTextView.text as NSString?)!
+        let stringText = NSMutableAttributedString(string: descriptionTextView.text)
+        stringText.addAttribute(NSAttributedStringKey.font, value: UIFont.italicSystemFont(ofSize: 12.0), range: descriptionText.range(of: "tagging the world!"))
+        descriptionTextView.attributedText = stringText
     }
+    
+    
     
     // MARK: Actions
     
     @IBAction func didTapSupportEmail(_ sender: Any) {
+        let mailComposeViewController = configuredMailComposeViewController()
+        if MFMailComposeViewController.canSendMail() {
+            self.present(mailComposeViewController, animated: true, completion: nil)
+        } else {
+            self.showSendMailErrorAlert()
+        }
     }
     
     @IBAction func didTapSwarduWebsite(_ sender: Any) {
+        openURLInBrowser("https://www.swardu.com/")
     }
     
     @IBAction func didTapReadMore(_ sender: Any) {
     }
     
     @IBAction func didTapCwWebsite(_ sender: Any) {
+        openURLInBrowser("http://www.creativeworkline.com/?ref=si")
     }
     
     @IBAction func didTapTermsOfUse(_ sender: Any) {
+        openURLInBrowser("https://www.swardu.com/terms")
     }
     
     @IBAction func didTapPrivacyPolicy(_ sender: Any) {
+        openURLInBrowser("https://www.swardu.com/privacy")
+    }
+    
+    private func openURLInBrowser(_ urlString: String) {
+        guard let url = URL(string: urlString) else {
+            return
+        }
+        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+    }
+    
+    // MARK: Mail
+    func configuredMailComposeViewController() -> MFMailComposeViewController {
+        let mailComposerVC = MFMailComposeViewController()
+        mailComposerVC.mailComposeDelegate = self // Extremely important to set the --mailComposeDelegate-- property, NOT the --delegate-- property
+        
+        mailComposerVC.setToRecipients(["support@swardu.com"])
+        mailComposerVC.setSubject("swardu iOS support request")
+        
+        return mailComposerVC
+    }
+    
+    func showSendMailErrorAlert() {
+        let sendMailErrorAlertViewController = UIAlertController(title: "Could Not Send Email", message: "Your device could not send e-mail.  Please check e-mail configuration and try again.", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        sendMailErrorAlertViewController.addAction(okAction)
+        self.present(sendMailErrorAlertViewController, animated: true, completion: nil)
+    }
+}
+
+// MARK: MFMailComposeViewControllerDelegate Method
+
+extension ViewController: MFMailComposeViewControllerDelegate {
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        
     }
 }
 
